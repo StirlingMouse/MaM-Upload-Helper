53dddb4285ef674efb7e5bb2f6039c0b50b27c84
Stirling Mouse <181794392+StirlingMouse@users.noreply.github.com>
2025-11-08 00:16:16 +0100

    Bump version

--- upload-helper.user.js
+++ upload-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MaM Upload Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.2.0
 // @description  Adds other torrents, preview, check for creating new entities and more to the upload page
 // @author       Stirling Mouse
 // @match        https://www.myanonamouse.net/tor/upload.php
